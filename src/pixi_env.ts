91accb66b7e2ab627898d75a33f35a2970f94989
tomaszlal <lalewicz.tomasz@gmail.com>
2023-07-18 00:50:00 +0200

    fun with pixi

--- src/pixi_env.ts
+++ src/pixi_env.ts
@@ -1,25 +1,88 @@
 import * as PIXI from "pixi.js";
 
 
+
 export class GameManager {
     private app:PIXI.Application;
-    public tetrisBlock2:PIXI.Sprite = new PIXI.Sprite();
+    public tetrisBlock1:PIXI.Sprite = new PIXI.Sprite;
+    public tetrisBlock2:PIXI.Sprite = new PIXI.Sprite;
+    public loaderPixi:PIXI.Loader = new PIXI.Loader();
+    public sprites: Array<PIXI.Sprite> = [];
 
     public constructor(){
         this.app = new PIXI.Application({width:600, height:600, backgroundColor:0xEEEEEE});
         
         document.body.appendChild(this.app.view);
 
-        console.log(this.app);
+        this.init();
+     }
 
+    public addTicker(): void {
+        this.app.ticker.add((delta) =>{
+            this.tetrisBlock2.rotation += 0.01;
+            this.tetrisBlock1.rotation += 0.005;
+        })
+       
+    }
+    
+    public  init(){
+
+        this.loaderPixi.add('tet1','/assets/images/tet_1.png')
+            .add('tet2','/assets/images/tet_2.png')
+            .load();
+
+        this.loaderPixi.onComplete.add((resources)=>{
+            console.log(resources.resources);
+
+            // resources.resources.
+            // conveert objects from resourses to aray
+            let resourcesArray = Object.keys(resources.resources).map((key) =>[key, resources.resources[key]]);
+            console.log(resourcesArray);
+            
+            //generate sprites from resourses
+            resourcesArray.forEach(element =>{
+                console.log(element[1].texture);
+                this.sprites.push(PIXI.Sprite.from(element[1].texture));
+            })
+
+            console.log("Sprites loaded");
+            console.log(this.sprites);
+
+            this.sprites.forEach(element => {
+                element.interactive = true;
+                element.anchor.set(0.5);
+                element.x = Math.random() * this.app.screen.width;
+                element.y = Math.random() * this.app.screen.height;
+                element.on("pointertap", () =>{
+                    element.rotation+= Math.PI/2;
+                })
+                this.app.stage.addChild(element);
+            })
+            
+
+
+
+        });
+       
+        // first block tetris
         let texture = PIXI.Texture.from('/assets/images/tet_1.png');
-        const tetrisBlock = PIXI.Sprite.from(texture);
-        tetrisBlock.anchor.x = 0.5;
-        tetrisBlock.anchor.y = 0.5;
-        tetrisBlock.x = this.app.screen.width / 2;
-        tetrisBlock.y = this.app.screen.height / 2;
-        this.app.stage.addChild(tetrisBlock);
+        this.tetrisBlock1 = PIXI.Sprite.from(texture);
+        this.tetrisBlock1.anchor.x = 0.5;
+        this.tetrisBlock1.anchor.y = 0.5;
+        this.tetrisBlock1.x = this.app.screen.width / 2;
+        this.tetrisBlock1.y = this.app.screen.height / 2;
+        this.app.stage.addChild(this.tetrisBlock1);
 
+      
+        this.tetrisBlock1.interactive=true;
+        this.tetrisBlock1.on('pointertap', ()=> {
+            this.tetrisBlock1.y += 100;
+            if (this.tetrisBlock1.y >= this.app.screen.height){
+                this.tetrisBlock1.y = 100;
+            }
+        })
+
+        // second block tetris
         texture = PIXI.Texture.from('/assets/images/tet_2.png');
         this.tetrisBlock2 = PIXI.Sprite.from(texture);
         this.tetrisBlock2.anchor.x = 0.5;
@@ -34,27 +97,6 @@ export class GameManager {
                 this.tetrisBlock2.x = 100;
             }
         })
-       
-        
-       
-
-       
-        
-
     }
-
-    public addTicker(): void {
-        this.app.ticker.add((delta) =>{
-
-            // console.log(this.app.stage.getChildAt(0));
-
-
-            this.app.stage.getChildAt(0).rotation += 0.02;
-            
-           
-        })
-       
-    }
-   
     
 }
